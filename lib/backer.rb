50da1d773d52fa85669b01bf2b1071de3acf7894
Robert Quintana <robertquintanatx@gmail.com>
2020-06-16 04:58:53 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,8 @@
 class Backer 
-  attr_reader :backed_projects
+  attr_reader :backed_projects, :name
   
-  def initialize
+  def initialize(name)
+    @name = name
     @backed_projects = []
   end 
   
